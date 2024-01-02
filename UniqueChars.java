72f9a45f3b290fc902d49904547c052f154378fe
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2024-01-02 17:06:24 +0200

    unigue chars works fine

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,7 +11,14 @@ public class UniqueChars {
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+        String answer = "";
+        char current = 0;
+        for (int i = 0; i < s.length(); i++) {
+            current = s.charAt(i);
+            if (current == ' ' || answer.indexOf(current) == -1) {
+                answer += current;
+            }
+        }
+        return answer;
     }
 }
