fdb1913cb78bd5639b255211277c2cc0cdb71d4f
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2024-01-02 17:31:17 +0200

    isleapyear ndaysinmonth functions work fine

--- Calendar0.java
+++ Calendar0.java
@@ -22,13 +22,15 @@ public class Calendar0 {
 
 	// Tests the nDaysInMonth function.
 	private static void nDaysInMonthTest(int year) {
-		// Replace this comment with your code
+		
 	}
 
 	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
-		return false;
+		boolean isLeapYear = false;
+		isLeapYear = ((year % 400) == 0) ||
+					 (((year % 4) == 0) && ((year % 100) != 0));
+		return isLeapYear;
 	}
 	 
 	// Returns the number of days in the given month and year.
@@ -36,8 +38,17 @@ public class Calendar0 {
 	// February has 28 days in a common year, and 29 days in a leap year.
 	// All the other months have 31 days.
 	public static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
-		return 0;
+		switch (month) {
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				return 30;
+			case 2:
+				return isLeapYear(year) ? 29 : 28;
+			default:
+				return 31;
+		}
 	}
 }
 
